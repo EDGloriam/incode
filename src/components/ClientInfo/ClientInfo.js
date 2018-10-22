3fa18acab54e1e4c4f53e941912d4442cbda6671
edgloriam <edgloriamm@gmail.com>
2018-10-23 02:59:13 +0300

    done

--- src/components/ClientInfo/ClientInfo.js
+++ src/components/ClientInfo/ClientInfo.js
@@ -11,28 +11,22 @@ const clientInfo = (props) => {
                         <Item.Image className='clientImage' src={props.client.general.avatar} bordered/>
                         <Item.Content className="textClient">
                             <Item.Header as='a' className="hedDetails">{props.client.general.firstName + ' ' + props.client.general.lastName}</Item.Header>
-                            <Item.Extra className="jobDetails">
-                                <p>
-                                    <span>Job:</span>
-                                    <Item>{props.client.job.title}</Item>
-                                    <Item>{props.client.job.company}</Item>
-                                </p>
-                            </Item.Extra>
-                            <Item.Description className="descripDetails">
-                                <p>
-                                    <span>Contacts:</span>
-                                    <Item>{props.client.contact.phone}</Item>
-                                    <Item>props.client.contact.email</Item>
-                                </p>
+                            <Item.Description className="jobDetails">
+                                <span>Job:</span>
+                                <Item>{props.client.job.title}</Item>
+                                <Item>{props.client.job.company}</Item>
                             </Item.Description>
                             <Item.Description className="descripDetails">
-                                <p>
-                                    <span>Addres:</span>
-                                    <Item>{props.client.address.country}</Item>
-                                    <Item>{props.client.address.city}</Item>
-                                    <Item>{props.client.address.street}</Item>
-                                    <Item>{props.client.address.zipCode}</Item>
-                                </p>
+                                <span>Contacts:</span>
+                                <Item>{props.client.contact.phone}</Item>
+                                <Item>props.client.contact.email</Item>
+                            </Item.Description>
+                            <Item.Description className="descripDetails">
+                                <span>Addres:</span>
+                                <Item>{props.client.address.country}</Item>
+                                <Item>{props.client.address.city}</Item>
+                                <Item>{props.client.address.street}</Item>
+                                <Item>{props.client.address.zipCode}</Item>
                             </Item.Description>
                             </Item.Content>
                     </Item>
